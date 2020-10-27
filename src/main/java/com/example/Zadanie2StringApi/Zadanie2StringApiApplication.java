c820f1e90f4e14c57093b9bfa0b66a8cd05f9fd9
mateusz-git <215851@edu.p.lodz.pl>
2020-10-27 14:25:52 +0100

    Created procject by Spring Initializr

---
+++ src/main/java/com/example/Zadanie2StringApi/Zadanie2StringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Zadanie2StringApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie2StringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zadanie2StringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Zadanie2StringApi/Zadanie2StringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Zadanie2StringApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie2StringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
