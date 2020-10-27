d49b83e25ce6fcc5275a3dfe8bc55c2b0bc68209
mateusz-git <215851@edu.p.lodz.pl>
2020-10-27 14:41:00 +0100

    Created endpoint

--- src/main/java/com/example/Zadanie2StringApi/Controller.java
+++ src/main/java/com/example/Zadanie2StringApi/Controller.java
@@ -1,7 +1,13 @@
 package com.example.Zadanie2StringApi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    @GetMapping("/showDetails")
+    public Details showDetails(@RequestParam String word) {
+        return null;
+    }
 }
