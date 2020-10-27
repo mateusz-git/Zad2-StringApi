abac764a7abd66ce1b56924c807266fed072ec90
mateusz-git <215851@edu.p.lodz.pl>
2020-10-27 14:38:05 +0100

    Created Details Constructor

--- src/main/java/com/example/Zadanie2StringApi/Details.java
+++ src/main/java/com/example/Zadanie2StringApi/Details.java
@@ -1,3 +1,15 @@
 package com.example.Zadanie2StringApi;
 
-public class Details {}
+public class Details {
+    private int upperCase;
+    private int lowerCase;
+    private int digitCase;
+    private int specialCase;
+
+    public Details(int upperCase, int lowerCase, int digitCase, int specialCase) {
+        this.upperCase = upperCase;
+        this.lowerCase = lowerCase;
+        this.digitCase = digitCase;
+        this.specialCase = specialCase;
+    }
+}
