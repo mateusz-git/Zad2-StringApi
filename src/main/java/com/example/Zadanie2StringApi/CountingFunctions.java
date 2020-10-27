1dc4d0f28a20a85948f50fada11e3659d3692517
mateusz-git <215851@edu.p.lodz.pl>
2020-10-27 15:03:31 +0100

    Created function to countig upper and lower Case ,digit and special character

--- src/main/java/com/example/Zadanie2StringApi/CountingFunctions.java
+++ src/main/java/com/example/Zadanie2StringApi/CountingFunctions.java
@@ -1,4 +1,24 @@
 package com.example.Zadanie2StringApi;
 
 public class CountingFunctions {
+    public static Details count(String word) {
+        int amountUpperCase = 0;
+        int amountLowerCase = 0;
+        int amountDigit = 0;
+        int amountSpecial = 0;
+
+        char[] charArray = word.toCharArray();
+
+        for (char c : charArray) {
+            if (Character.isUpperCase(c))
+                amountUpperCase++;
+            else if (Character.isLowerCase(c))
+                amountLowerCase++;
+            else if (Character.isDigit(c))
+                amountDigit++;
+            else
+                amountSpecial++;
+        }
+        return new Details(amountUpperCase, amountLowerCase, amountDigit, amountSpecial);
+    }
 }
